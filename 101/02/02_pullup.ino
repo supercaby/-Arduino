5c39c26944f3f69a35e17066dafaae33c75f2383
supercaby <48729053+supercaby@users.noreply.github.com>
2019-09-26 15:44:45 +0800

    Create 02_pullup.ino

---
+++ 101/02/02_pullup.ino
@@ -0,0 +1,15 @@
+int ledPin = 13;
+int buttonPin = 2;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);\\设置端口上拉
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == 0) {
+    digitalWrite(ledPin, 1);
+  }
+  else {
+    digitalWrite(ledPin, 0);
+  }
+}
