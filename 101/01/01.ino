55eb5ecbf44cf69d177cffefe5a163aecc963e81
supercaby <48729053+supercaby@users.noreply.github.com>
2019-09-26 15:06:54 +0800

    Update 01.ino

--- 101/01/01.ino
+++ 101/01/01.ino
@@ -1,15 +1,15 @@
 int redLed = 3;
 int greenLed = 4;
 void setup() {
-pinMode(redLed,OUTPUT);
-pinMode(greenLed,OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
 }
 
 void loop() {
-digitalWrite(redLed,HIGH);
-digitalWrite(greenLed,LOW);
-delay(500);
-digitalWrite(redLed,LOW);
-digitalWrite(greenLed,HIGH);
-delay(500);
+  digitalWrite(redLed, HIGH);
+  digitalWrite(greenLed, LOW);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  digitalWrite(greenLed, HIGH);
+  delay(50);
 }
